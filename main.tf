194c72098f893cbdff0409dc0faf5a1f53bae2b5
abdyusma <abdul.rahman.yusmadi@crayon.com>
2023-06-19 01:44:37 +0800

    fix main record

--- main.tf
+++ main.tf
@@ -140,7 +140,7 @@ data "aws_route53_zone" "main" {
 }
 
 resource "aws_route53_record" "main" {
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
   name    = "rahmandemo.com"
   type    = "A"
 
