b5c9070dbc7efa652c13e341052108cc1fefb4af
abdyusma <abdul.rahman.yusmadi@crayon.com>
2023-06-19 00:34:29 +0800

    Update setup-nginx.sh
    with comments

--- scripts/setup-nginx.sh
+++ scripts/setup-nginx.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+# update amazon linux
 yum update -y
+
+# install nginx
 yum install nginx -y
+
+# customize nginx page
+sudo sed -i "s/Welcome to/$(hostname) uses/" /usr/share/nginx/html/index.html
+
+# start nginx
 systemctl start nginx
+
+# start nginx during boot
 sudo systemctl enable nginx
